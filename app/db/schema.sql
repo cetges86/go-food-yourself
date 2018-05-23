2552e0d7f357745b69a3c4adc10b2c851b9598fc
trevorogg <ogg.trevor@gmail.com>
2018-05-22 19:26:07 -0600

    adding lots of meat yo

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,14 +1,53 @@
 CREATE DATABASE foods_db;
 USE foods_db;
 
-INSERT INTO Ingredients (name, category, important)
-VALUES ("Bread", "Grains", true);
+-- Poultry
 
 INSERT INTO Ingredients (name, category, important)
-VALUES ("Milk", "Dairy", true);
+VALUES ("Chicken Breast", "Protein", true);
 
 INSERT INTO Ingredients (name, category, important)
-VALUES ("Butter", "Dairy", true);
+VALUES ("Chicken Thighs", "Protein", true);
 
 INSERT INTO Ingredients (name, category, important)
-VALUES ("Cheddar Cheese", "Dairy", true);
+VALUES ("Ground Turkey", "Protein", true);
+
+--Beef
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Ground Beef", "Protein", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Steak", "Protein", true);
+
+-- Wild Card
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Hot Dogs", "Protein", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Italian Sausage", "Protein", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Pepperoni", "Protein", true);
+
+-- Pork
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Ham", "Protein", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Bacon", "Protein", true);
+
+-- Vegetarian
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Tofu", "Protein", true);
+
+-- Fish
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Salmon", "Protein", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Tuna", "Protein", true);
