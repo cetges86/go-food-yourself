479aad48363b3ced764d8bc5777d7752cc405f31
Christopher Etges <c.etges86@gmail.com>
2018-05-22 19:39:31 -0600

    added grains

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,8 +1,6 @@
 CREATE DATABASE foods_db;
 USE foods_db;
 
-INSERT INTO Ingredients (name, category, important)
-VALUES ("Bread", "Grains", true);
 
 INSERT INTO Ingredients (name, category, important)
 VALUES ("Milk", "Dairy", true);
@@ -12,3 +10,81 @@ VALUES ("Butter", "Dairy", true);
 
 INSERT INTO Ingredients (name, category, important)
 VALUES ("Cheddar Cheese", "Dairy", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Bread", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("White Rice", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Brown Rice", "Grains", true);
+
+ INSERT INTO Ingredients (name, category, important)
+VALUES ("Rice", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Corn", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Oats", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Popcorn", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Wheat", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Barley", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Flour", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Bagel", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Roll", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Cornbread", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Corn tortilla", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Crackers", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Couscous", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Flour tortillas", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Noodles", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Pretzels", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Hamburger Buns", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Hotdog Buns", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Spaghetti", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Macaroni", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Corn Flakes", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Grits", "Grains", true);
+
+INSERT INTO Ingredients (name, category, important)
+VALUES ("Pitas", "Grains", true);
