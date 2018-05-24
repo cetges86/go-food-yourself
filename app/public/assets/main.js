06be3c01df2b50746de343aee2f5515bdf2a55b8
LEi <LEIVC@github.com>
2018-05-24 17:22:17 -0600

    produce list

--- app/public/assets/main.js
+++ app/public/assets/main.js
@@ -1,17 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.autocomplete');
-    var instances = M.Autocomplete.init(elems, options);
-  });
+$(document).ready(function(){
+  $('select').formSelect();
+});
 
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('input.autocomplete').autocomplete({
-      data: {
-        "Apple": null,
-        "Microsoft": null,
-        "Google": 'https://placehold.it/250x250'
-      },
-    });
-  });
