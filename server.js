5f696157b03ce707ae2eeedd729f7471edf19aeb
Christopher Etges <c.etges86@gmail.com>
2018-05-30 21:41:12 -0600

    fix html route

--- server.js
+++ server.js
@@ -16,25 +16,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  layoutsDir: path.join(__dirname, "/app/views/layouts/"),
-  partialsDir: path.join(__dirname, "/app/views/partials/")
-}));
-
-app.set("view engine", "handlebars");
-app.set('views', path.join(__dirname, "/app/views"));
-// app.set('partials', path.join(__dirname, "/app/views/partials"))
 
 // Import routes and give the server access to them.
 require("./app/routes/apiroutes")(app);
 require("./app/routes/htmlroutes")(app);
 
 // Start our server so that it can begin listening to client requests.
-
 db.sequelize.sync().then(function(){
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
