958cad543f796782c4de8436184a3d2826a4e3de
trevorogg <ogg.trevor@gmail.com>
2018-05-31 20:41:56 -0600

    post recipe

--- app/public/assets/main.js
+++ app/public/assets/main.js
@@ -1,36 +1,37 @@
 $(document).ready(function () {
   $('.parallax').parallax();
-});
-//  parallax initilization with Jquery
+
+  //  parallax initilization with Jquery
+
 
   $.get("/api/ingredients", function (data) {
 
     console.log(data);
-    
+
     for (var i = 0; i < data.length; i++) {
       category = data[i].category;
       if (category === "Protein") {
         $('#ing').append(`
-          <option value="${i+1}">${data[i].name}</option>`);
+          <option value="${data[i].id}">${data[i].name}</option>`);
       };
 
       if (category === "Dairy") {
         $('#dairy').append(`
-        <option value="${i+1}">${data[i].name}</option>`);
+        <option value="${data[i].id}">${data[i].name}</option>`);
       };
       if (category === "Produce") {
         $('#produce').append(`
-        <option value="${i+1}">${data[i].name}</option>`);
+        <option value="${data[i].id}">${data[i].name}</option>`);
       };
       if (category === "Grains") {
         $('#grains').append(`
-        <option value="${i+1}">${data[i].name}</option>`);
+        <option value="${data[i].id}">${data[i].name}</option>`);
       };
       if (category === "Spices/Seasoning/Condiments") {
         $('#seasoning').append(`
-        <option value="${i+1}">${data[i].name}</option>`);
+        <option value="${data[i].id}">${data[i].name}</option>`);
       };
-      
+
     }
     $('select').formSelect();
   });
@@ -54,9 +55,9 @@ $(document).ready(function () {
     $.get("/search/", reqObj).
       then(function (res) {
         console.log(res)
-      
-        for(let i = 0; i < res.length; i++){
-          if (res[i].Ingredients.length != 0){
+
+        for (let i = 0; i < res.length; i++) {
+          if (res[i].Ingredients.length != 0) {
             $('#results').append("<h1> Recipe Name: " + res[i].name + "</h1>")
             $('#results').append("<h4> Number of Ingredients you have: " + res[i].Ingredients.length + "</h4>")
 
@@ -65,3 +66,55 @@ $(document).ready(function () {
       })
 
   })
+
+  $('#post').on('click', function (event) {
+    event.preventDefault();
+    $('select').formSelect();
+
+    $.get("/api/ingredients", function (data) {
+      let ingredients = data;
+    })
+
+    let dairy = $('#dairy').val()
+    let protein = $('#ing').val()
+    let seasoning = $('#seasoning').val()
+    let grains = $('#grains').val()
+    let produce = $('#produce').val()
+
+    let ings = [dairy.concat(protein).concat(seasoning).concat(grains).concat(produce)];
+    console.log(ings)
+
+    let name = $('#name').val();
+    let numberOfIng = $('#numberOfIng').val();
+    let link = $('#link').val();
+
+    ingArray = [];
+    ings.forEach(ing => {
+      let recipe_ing = {
+        name: ing.name,
+        category: ing.category,
+        important: ing.important
+      }
+
+      ingArray.push(recipe_ing);
+
+    });
+
+    const reqObj = {
+      name: name,
+      numberOfIng: numberOfIng,
+      link: link,
+      Ingredients: ingArray
+    }
+
+    console.log(reqObj);
+
+    $.post("/api/recipe", reqObj)
+      .then(function(res) {
+        console.log(res);
+      })
+
+
+
+  })
+});
