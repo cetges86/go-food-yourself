0ec8d0d5f891372ad06dc0b359f97babb9f58deb
Christopher Etges <c.etges86@gmail.com>
2018-05-31 20:01:37 -0600

    add recipe works

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -1,17 +1,20 @@
-module.exports = function(sequelize, DataTypes) {
-    const Recipe = sequelize.define("Recipe", {
-      name: DataTypes.STRING,
-      numberOfIng: DataTypes.INTEGER,
-      link: DataTypes.STRING
-    }, {
+module.exports = function (sequelize, DataTypes) {
+  const Recipe = sequelize.define("Recipe", {
+    name: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    numberOfIng: DataTypes.INTEGER,
+    link: DataTypes.STRING
+  }, {
       timestamps: false
     });
 
-    Recipe.associate = function (models) {
-      Recipe.belongsToMany(models.Ingredients, {
-        through: 'ingredient_recipes',
-        foreignKey: 'ingredient_id'
-      });
-    };
-    return Recipe;
+  Recipe.associate = function (models) {
+    Recipe.belongsToMany(models.Ingredients, {
+      through: 'ingredient_recipes',
+      foreignKey: 'ingredient_id'
+    });
   };
+  return Recipe;
+};
